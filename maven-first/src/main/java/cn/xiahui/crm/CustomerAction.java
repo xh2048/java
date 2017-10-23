c286482432275053dabc86e4b69f134b32344bae
xh2016 <xh2048@gmail.com>
2017-10-23 19:54:27 +0800

    maven第一天

---
+++ maven-first/src/main/java/cn/xiahui/crm/CustomerAction.java
@@ -0,0 +1,22 @@
+package cn.xiahui.crm;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class CustomerAction extends ActionSupport{
+	 
+	private Long custId;
+
+	public Long getCustId() {
+		return custId;
+	}
+
+	public void setCustId(Long custId) {
+		this.custId = custId;
+	}
+	
+	public String findCustomerById(){
+		System.out.println("前端传过来的客户id是："+custId);
+		
+		return SUCCESS;
+	}
+}
