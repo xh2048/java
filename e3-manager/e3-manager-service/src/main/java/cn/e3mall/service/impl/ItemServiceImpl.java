22047a444a6ec67fcf71fcf439821c6014382d29
xh2016 <xh2048@126.com>
2017-11-28 16:19:31 +0800

    e3商城第四天止

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.e3mall.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,12 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.common.utils.IDUtils;
+import cn.e3mall.mapper.TbItemDescMapper;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.pojo.TbItemExample.Criteria;
 import cn.e3mall.service.ItemService;
@@ -20,6 +25,8 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
 
 	@Override
 	public TbItem getItemById(long itemId) {
@@ -54,6 +61,31 @@ public class ItemServiceImpl implements ItemService {
 		 result.setTotal(total);
 		 
 		 return result;
+	}
+
+	@Override
+	public E3Result addItem(TbItem item, String desc) {
+		//生成商品id
+		long itemId = IDUtils.genItemId();
+		//补全item的属性
+		item.setId(itemId);
+		//1-正常、2-下架、3-删除
+		item.setStatus((byte) 1);
+		item.setCreated(new Date());
+		item.setUpdated(new Date());
+		//向商品描述表插入数据
+		itemMapper.insert(item);
+		//创建一个商品描述表对应的pojo对象
+		TbItemDesc itemDesc = new TbItemDesc();
+		//补全属性
+		itemDesc.setItemId(itemId);
+		itemDesc.setItemDesc(desc);
+		itemDesc.setCreated(new Date());
+		itemDesc.setUpdated(new Date());
+		//想商品描述表出入数据
+		itemDescMapper.insert(itemDesc);
+		//返回成功
+		return E3Result.ok();
 	}
 
 }
