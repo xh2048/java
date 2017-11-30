2bce2b5e3dd5e96bea59a44def942c1673db0bf4
xh2016 <xh2048@126.com>
2017-11-30 17:30:18 +0800

    e3商城项目第6天止

--- e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
+++ e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
@@ -3,10 +3,14 @@ package cn.e3mall.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import cn.e3mall.common.jedis.JedisClient;
 import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.common.utils.JsonUtils;
 import cn.e3mall.content.service.ContentService;
 import cn.e3mall.mapper.TbContentMapper;
 import cn.e3mall.pojo.TbContent;
@@ -22,6 +26,12 @@ public class ContentServiceImpl implements ContentService {
 	
 	@Autowired
 	private TbContentMapper contentMapper;
+	
+	@Value("${CONTENT_LIST}")
+	private String CONTENT_LIST;
+
+	@Autowired
+	private JedisClient jedisClient;
 
 	@Override
 	public E3Result addContent(TbContent content) {
@@ -30,6 +40,8 @@ public class ContentServiceImpl implements ContentService {
 		content.setUpdated(new Date());
 		//插入到数据库
 		contentMapper.insert(content);
+		//缓存同步，删除缓存中对应的数据
+		jedisClient.hdel(CONTENT_LIST, content.getCategoryId().toString());
 		return E3Result.ok();
 	}
 /**
@@ -37,12 +49,31 @@ public class ContentServiceImpl implements ContentService {
  */
 	@Override
 	public List<TbContent> getContentListByCid(long cid) {
+		//查询缓存
+		try {
+			//如果缓存中有直接响应结果
+			String json = jedisClient.hget(CONTENT_LIST, cid+"");
+			if(StringUtils.isNotBlank(json)){
+				List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+				return list;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//如果没有查询数据库
 		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
 		//设置查询条件
 		criteria.andCategoryIdEqualTo(cid);
 		//执行查询
 		List<TbContent> list = contentMapper.selectByExampleWithBLOBs(example);
+		//把结果添加到缓存
+		try {
+			jedisClient.hset(CONTENT_LIST, cid+"", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		return list;
 	}
 
