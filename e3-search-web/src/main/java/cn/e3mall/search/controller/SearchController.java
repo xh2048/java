0123b63104d5985951cf29ee3cc3cb4743f9e55c
xh2016 <xh2048@gmail.com>
2017-12-02 14:42:16 +0800

    e3商城项目第8天止

--- e3-search-web/src/main/java/cn/e3mall/search/controller/SearchController.java
+++ e3-search-web/src/main/java/cn/e3mall/search/controller/SearchController.java
@@ -37,6 +37,9 @@ public class SearchController {
 		model.addAttribute("recourdCount",searchResult.getRecordCount());
 		model.addAttribute("itemList",searchResult.getItemList());
 		
+		//异常测试
+//		int a = 1/0;
+		
 		//返回逻辑视图
 		return "search";
 		
